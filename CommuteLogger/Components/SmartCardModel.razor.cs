d49903fc0e4461f8b5d0df24a2324223d3849861
iusami_hacarus <ippei@hacarus.com>
2023-05-04 21:54:29 +0900

    fist commit

---
+++ CommuteLogger/Components/SmartCardModel.razor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using PCSC;
+
+namespace CommuteLogger.Components;
+
+public partial class SmartCardModel
+{
+    public string[] readerNames { get; set; } = Array.Empty<string>();
+
+    [Parameter]
+    public EventCallback<string[]> ChangeReaderNames { get; set; }
+
+    public void ListupReaders()
+    {
+        var contextFactory = ContextFactory.Instance;
+        var context = contextFactory.Establish(SCardScope.System);
+        
+        Console.WriteLine("Currently connected readers: ");
+        readerNames = context.GetReaders();
+        foreach (var readerName in readerNames)
+        {
+            Console.WriteLine("\t" + readerName);
+        }
+        ChangeReaderNames.InvokeAsync(this.readerNames);
+        
+    }
+}
+
